b8b2583d185a802026b7cbcbbd09285334919de2
Ayda Karimzadeh <akarimzade257@gmail.com>
2026-03-08 22:45:33 +0330

    8th March exercise

---
+++ Personal-exercises/TypeError3.py
@@ -0,0 +1,8 @@
+# Please try to guess what is missing in the following code and add the missing part so that the code works fine.
+# import math
+# print(math.pow(2))
+
+import math
+print(math.pow(2,2))
+
+# you simply needed to pass a second argument so that pow can calculate the power of the first argument.
