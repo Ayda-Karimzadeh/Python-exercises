855346a4828da73d5e9639065af5f5679caa2204
Ayda Karimzadeh <akarimzade257@gmail.com>
2026-05-29 04:38:02 +0330

    29th May exercise

---
+++ Personal-exercises/UsernameAndPasswordChecker/UsernameAndPasswordChecker.py
@@ -0,0 +1,30 @@
+filepath = "D:/Python developer/Python-exercises/Personal-exercises/UsernameAndPasswordChecker/users.txt"
+while True:
+    usr = input("Enter username: ")
+
+    with open(filepath,"r") as f:
+       users = f.readlines()
+       users = [i.strip() for i in users]
+    if usr in users:
+        print("username exists.")
+        continue
+    else:
+        print("username is fine")
+        break
+
+while True:
+    notes = []
+    psw = input("Enter password: ")
+    if not any(i.isdigit() for i in psw):
+        notes.append("You need at least one number")
+    if not any(i.isupper() for i in psw):
+        notes.append("You need at least one uppercase letter")
+    if len(psw) < 5:
+        notes.append("You need at least 5 characters")
+    if len(notes) == 0:
+        print("Password is fine")
+        break
+    else:
+        print("Please check the following : ")
+        for note in notes:
+            print(note)
